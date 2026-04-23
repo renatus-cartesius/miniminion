31e45cce797f540f4c38a2003cd0105c5a884229
renatus-cartesius <cartesius.absolute@gmail.com>
2026-04-23 09:12:41 +0300

    chore: added resource definition

--- src/main.zig
+++ src/main.zig
@@ -42,32 +42,32 @@ pub fn main() !void {
     }
 }
 
-pub fn parseResourses(allocator: std.mem.Allocator, json_input: []const u8) !std.StringArrayHashMap(Resource) {
-    const parsed = try std.json.parseFromSlice(std.json.Value, allocator, json_input, .{});
-    defer parsed.deinit();
-
-    var resources = std.StringArrayHashMap(Resource).init(allocator);
-    errdefer {
-        resources.deinit();
-    }
-
-    const root_obj = parsed.Value.object;
-
-    var iter = root_obj.iterator();
-    while (iter.next()) |entry| {
-        const key = entry.key_ptr.*;
-        const val = entry.value_ptr.object;
-
-        const res_type = val.get("type").?.string;
-
-        if (std.mem.eql(u8, res_type, "file")) {
-            const content = try allocator.dupe(u8, val.get("content").?.string);
-            try resources.put(try allocator.dupe(u8, key), .{ .file = .{ .content = content } });
-        } else if (std.mem.eql(u8, res_type, "package")) {
-            const name = try .allocator.dupe(u8, val.get("name").?.string);
-            try resources.put(try allocator.dupe(u8, key), .{ .package = .{ .name = name } });
-        }
-    }
-
-    return resources;
-}
+// pub fn parseResourses(allocator: std.mem.Allocator, json_input: []const u8) !std.StringArrayHashMap(Resource) {
+//     const parsed = try std.json.parseFromSlice(std.json.Value, allocator, json_input, .{});
+//     defer parsed.deinit();
+//
+//     var resources = std.StringArrayHashMap(Resource).init(allocator);
+//     errdefer {
+//         resources.deinit();
+//     }
+//
+//     const root_obj = parsed.Value.object;
+//
+//     var iter = root_obj.iterator();
+//     while (iter.next()) |entry| {
+//         const key = entry.key_ptr.*;
+//         const val = entry.value_ptr.object;
+//
+//         const res_type = val.get("type").?.string;
+//
+//         if (std.mem.eql(u8, res_type, "file")) {
+//             const content = try allocator.dupe(u8, val.get("content").?.string);
+//             try resources.put(try allocator.dupe(u8, key), .{ .file = .{ .content = content } });
+//         } else if (std.mem.eql(u8, res_type, "package")) {
+//             const name = try .allocator.dupe(u8, val.get("name").?.string);
+//             try resources.put(try allocator.dupe(u8, key), .{ .package = .{ .name = name } });
+//         }
+//     }
+//
+//     return resources;
+// }
--- src/resource.zig
+++ src/resource.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+const ResourceErrors = error{
+    MissingType,
+};
+
 pub const File = struct {
     path: []const u8,
     content: []const u8 = "",
@@ -17,5 +21,61 @@ pub const Resource = union(enum) {
 };
 
 pub const State = struct {
-    resources: []*Resource,
+    resources: std.ArrayList(Resource),
+    allocator: std.mem.Allocator,
+    json_data: ?std.json.Parsed(std.json.Value) = null,
+
+    pub fn init(allocator: std.mem.Allocator) State {
+        return .{
+            .allocator = allocator,
+            .resources = std.ArrayList(Resource).initCapacity(allocator, 0),
+        };
+    }
+
+    pub fn deinit(self: *State) void {
+        if (self.json_data) |p| p.deinit();
+        self.resources.deinit(self.allocator);
+    }
+
+    pub fn load(self: *State, json: []const u8) !void {
+        if (self.json_data) |p| p.deinit();
+        self.resources.clearRetainingCapacity();
+
+        self.json_data = try std.json.parseFromSlice(std.json.Value, self.allocator, json, .{});
+
+        var iter = self.json_data.?.value.object.iterator();
+        while (iter.next()) |entry| {
+            const resource_name = entry.key_ptr.*;
+            const resource_value = entry.value_ptr.*;
+
+            const resource_type = resource_value.object.get("type").?.string;
+            //
+            // var found = false;
+            // inline for(std.meta.eql(resource_type, ))
+
+            std.debug.print("Resource Name: {s}, Type: {}\n", .{ resource_name, resource_type });
+        }
+    }
 };
+
+test "State map parsing" {
+    const allocator = std.testing.allocator;
+    var state = State.init(allocator);
+    defer state.deinit();
+
+    const json =
+        \\{
+        \\  "setup-shell": {
+        \\    "type": "file",
+        \\    "path": "/home/user/.zshrc",
+        \\    "content": "alias z=zig"
+        \\  },
+        \\  "compiler": {
+        \\    "type": "package",
+        \\    "name": "zig"
+        \\  }
+        \\}
+    ;
+
+    try state.load(json);
+}
