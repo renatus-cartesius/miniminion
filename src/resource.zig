4d757cdeff2bbb1c26fb3bccb29e6b71b2586745
renatus-cartesius <cartesius.absolute@gmail.com>
2026-04-27 10:24:15 +0300

    chore(resource): updated deserialinzing

--- src/resource.zig
+++ src/resource.zig
@@ -15,53 +15,49 @@ pub const Package = struct {
     version: ?[]const u8 = null,
 };
 
-pub const Resource = union(enum) {
+pub const ResourceData = union(enum) {
     file: File,
     package: Package,
 };
 
-pub const State = struct {
-    resources: std.ArrayList(Resource),
-    allocator: std.mem.Allocator,
-    json_data: ?std.json.Parsed(std.json.Value) = null,
-
-    pub fn init(allocator: std.mem.Allocator) State {
-        return .{
-            .allocator = allocator,
-            .resources = std.ArrayList(Resource).initCapacity(allocator, 0),
-        };
-    }
-
-    pub fn deinit(self: *State) void {
-        if (self.json_data) |p| p.deinit();
-        self.resources.deinit(self.allocator);
-    }
-
-    pub fn load(self: *State, json: []const u8) !void {
-        if (self.json_data) |p| p.deinit();
-        self.resources.clearRetainingCapacity();
-
-        self.json_data = try std.json.parseFromSlice(std.json.Value, self.allocator, json, .{});
-
-        var iter = self.json_data.?.value.object.iterator();
-        while (iter.next()) |entry| {
-            const resource_name = entry.key_ptr.*;
-            const resource_value = entry.value_ptr.*;
-
-            const resource_type = resource_value.object.get("type").?.string;
-            //
-            // var found = false;
-            // inline for(std.meta.eql(resource_type, ))
-
-            std.debug.print("Resource Name: {s}, Type: {}\n", .{ resource_name, resource_type });
-        }
-    }
+pub const Resource = struct {
+    name: []const u8,
+    data: ResourceData,
 };
 
+fn parseReources(allocator: std.mem.Allocator, json: []const u8) ![]Resource {
+    const parsed = try std.json.parseFromSlice(std.json.Value, allocator, json, .{});
+    // defer parsed.deinit();
+
+    var resources = try std.ArrayList(Resource).initCapacity(allocator, 0);
+    // defer resources.deinit(allocator);
+
+    var iter = parsed.value.object.iterator();
+    while (iter.next()) |entry| {
+        const resource_data = entry.value_ptr.*;
+        const resource_name = entry.key_ptr.*;
+
+        const type_field = resource_data.object.get("type") orelse return error.MissingType;
+        const type_name = type_field.string;
+
+        inline for (std.meta.fields(ResourceData)) |field| {
+            if (std.mem.eql(u8, type_name, field.name)) {
+                const parsed_field = try std.json.parseFromValue(field.type, allocator, resource_data, .{ .ignore_unknown_fields = true });
+                // defer parsed_field.deinit();
+
+                try resources.append(allocator, .{ .name = resource_name, .data = @unionInit(ResourceData, field.name, parsed_field.value) });
+                break;
+            }
+        }
+    }
+
+    return resources.toOwnedSlice(allocator);
+}
+
 test "State map parsing" {
-    const allocator = std.testing.allocator;
-    var state = State.init(allocator);
-    defer state.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
     const json =
         \\{
@@ -70,12 +66,26 @@ test "State map parsing" {
         \\    "path": "/home/user/.zshrc",
         \\    "content": "alias z=zig"
         \\  },
+        \\  "hosts-config": {
+        \\    "type": "file",
+        \\    "path": "/etc/hosts",
+        \\    "content": "asdf"
+        \\  },
         \\  "compiler": {
         \\    "type": "package",
-        \\    "name": "zig"
+        \\    "name": "zig",
+        \\    "version": "0.16"
         \\  }
         \\}
     ;
 
-    try state.load(json);
+    const resources = try parseReources(allocator, json);
+
+    for (resources) |r| {
+        std.debug.print("\nResource {s}:\n\t", .{@tagName(r.data)});
+        switch (r.data) {
+            .file => |f| std.debug.print("filepath: {s}, content: {s}\n", .{ f.path, f.content }),
+            .package => |p| std.debug.print("name: {s}, version: {s}\n", .{ p.name, p.version orelse "unset" }),
+        }
+    }
 }
