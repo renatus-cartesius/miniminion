6fef3bc82f9ddb8f28e4b31c1786e4be654725a7
renatus-cartesius <cartesius.absolute@gmail.com>
2026-05-02 01:12:03 +0300

    feat: trying to threat state as dag of resources

--- src/resource.zig
+++ src/resource.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
+const dag = @import("dag.zig");
 
-const ResourceErrors = error{
-    MissingType,
-};
+const ResourceErrors = error{ MissingType, NotFoundResource };
 
 pub const File = struct {
     path: []const u8,
@@ -23,9 +22,15 @@ pub const ResourceData = union(enum) {
 pub const Resource = struct {
     name: []const u8,
     data: ResourceData,
+    deps: []const []const u8 = &.{},
 };
 
-fn parseReources(arena: *std.heap.ArenaAllocator, json: []const u8) ![]Resource {
+pub const State = struct {
+    rdag: dag.DAG(Resource),
+    rmap: std.StringHashMap(usize),
+};
+
+pub fn parseReources(arena: *std.heap.ArenaAllocator, json: []const u8) ![]Resource {
     const allocator = arena.allocator();
     const parsed = try std.json.parseFromSlice(std.json.Value, allocator, json, .{});
 
@@ -39,11 +44,17 @@ fn parseReources(arena: *std.heap.ArenaAllocator, json: []const u8) ![]Resource 
         const type_field = resource_data.object.get("type") orelse return error.MissingType;
         const type_name = type_field.string;
 
+        const deps_value = resource_data.object.get("deps");
+        const deps = if (deps_value) |dv|
+            (try std.json.parseFromValue([]const []const u8, allocator, dv, .{})).value
+        else
+            @as([]const []const u8, &.{});
+
         inline for (std.meta.fields(ResourceData)) |field| {
             if (std.mem.eql(u8, type_name, field.name)) {
                 const parsed_field = try std.json.parseFromValue(field.type, allocator, resource_data, .{ .ignore_unknown_fields = true });
 
-                try resources.append(allocator, .{ .name = resource_name, .data = @unionInit(ResourceData, field.name, parsed_field.value) });
+                try resources.append(allocator, .{ .name = resource_name, .data = @unionInit(ResourceData, field.name, parsed_field.value), .deps = deps });
                 break;
             }
         }
@@ -52,21 +63,25 @@ fn parseReources(arena: *std.heap.ArenaAllocator, json: []const u8) ![]Resource 
     return resources.toOwnedSlice(allocator);
 }
 
-test "State map parsing" {
+test "Resource: simple state execution" {
+    const print = std.debug.print;
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
+    const allocator = arena.allocator();
 
     const json =
         \\{
         \\  "setup-shell": {
         \\    "type": "file",
         \\    "path": "/home/user/.zshrc",
-        \\    "content": "alias z=zig"
+        \\    "content": "alias z=zig",
+        \\    "deps": ["hosts-config", "compiler"]
         \\  },
         \\  "hosts-config": {
         \\    "type": "file",
         \\    "path": "/etc/hosts",
-        \\    "content": "asdf"
+        \\    "content": "asdfsdf",
+        \\    "deps": ["compiler"]
         \\  },
         \\  "compiler": {
         \\    "type": "package",
@@ -77,12 +92,39 @@ test "State map parsing" {
     ;
 
     const resources = try parseReources(&arena, json);
+    var rdag = try dag.DAG(Resource).init(allocator);
+    defer rdag.deinit();
+    var rmap = std.StringHashMap(usize).init(allocator);
+    defer rmap.deinit();
+
+    print("Parsed state:\n", .{});
 
     for (resources) |r| {
-        std.debug.print("Resource {s}:\n\t", .{@tagName(r.data)});
+        try rmap.put(r.name, try rdag.addNode(r));
+
+        print("Resource {s}:\n", .{r.name});
         switch (r.data) {
-            .file => |f| std.debug.print("filepath: {s}, content: {s}\n", .{ f.path, f.content }),
-            .package => |p| std.debug.print("name: {s}, version: {s}\n", .{ p.name, p.version orelse "unset" }),
+            .file => |f| print("\tfilepath: {s}, content: {s}\n", .{ f.path, f.content }),
+            .package => |p| print("\tname: {s}, version: {s}\n", .{ p.name, p.version orelse "unset" }),
+        }
+
+        for (r.deps) |d| {
+            print("\t\tDependency: {s}\n", .{d});
         }
     }
+
+    for (resources) |r| {
+        for (r.deps) |d| {
+            try rdag.addEdge(rmap.get(r.name) orelse return ResourceErrors.NotFoundResource, rmap.get(d) orelse return ResourceErrors.NotFoundResource);
+        }
+    }
+
+    const order = try rdag.topologicalSort(allocator);
+    defer allocator.free(order);
+
+    print("\nState execution order: ", .{});
+    for (order) |i| {
+        std.debug.print("{s} -> ", .{rdag.nodes.items[i].value.name});
+    }
+    std.debug.print("End.\n", .{});
 }
